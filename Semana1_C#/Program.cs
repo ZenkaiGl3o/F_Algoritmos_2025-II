fdf8bd265c9b8d5575a2050ea7819b2fb6bb6ef5
Gl3o <eolailocapo@gmail.com>
2025-09-02 15:30:38 -0500

    subiendo ejercicio 1-Sem1

---
+++ Semana1_C#/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Semana1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+        static void ejer1()  //Metodos: pedacitos de codigo que pueden llamarse a la rama principal
+        {
+            String nombre, carrera; //Declarando variables
+
+            Console.Write("Ingrese su nombre completo: ");
+            nombre = Console.ReadLine(); //readline para asignar lo escrito a la variable   
+            Console.Write("Ingrese la carrera que estudia: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\nHola {nombre}, bienvenido a Fundamentos de Algoritmos de {carrera}"); 
+            //El "$" es una concatenacion por interpolacion al inicio del texto para poner varias mierditas en vez del "+"
+            // para bajar de linea usar altGR + ? y n: \n
+        }
+        static void ejer2()
+        {
+
+        }
+
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ Semana1_Python/Semana1_Python.py
@@ -0,0 +1,8 @@
+﻿def ejer1():
+    nombre = input("Ingrese su name pe: ")  #Definir variable, y leer los datos (funcionan así xd)
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"Hola {nombre}, bienvenido a FA de {carrera}") 
+          #Imprimir es con print, y para concatenacion de interpolacion es una "f" antes de las comillas
+
+ejer1()
